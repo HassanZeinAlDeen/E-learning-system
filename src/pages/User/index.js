711b6d7714f260fe16bde55d33d6ebb03f637219
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-04-02 19:00:30 +0300

    adding user features

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -5,26 +5,27 @@ import Button from "../../components/Button";
 function User() {
 
   const path = () => {
-    window.location.href="http://localhost:3000";
-
+    window.location.href = "http://localhost:3000";
+    localStorage.clear();
   }
 
-
-  return(
-    <div className="body">
-    <div className="content">
-      <div className="logo">
-        <img id="logo" src={Mylogo} alt="logo.png" />
+  return (
+    <div className="newBody">
+      <div className="newContent">
+        <div className="logo">
+          <img id="logo" src={Mylogo} alt="logo.png" />
+        </div>
+        <div className="right">
+          <Button onClick={path}>Logout</Button>
+        </div>
       </div>
-      <div className="right">
-        <Button onClick={path}>Logout</Button>
+      <div id="results">
+        <div className="leftbar">
+          <a className="linkClasses">View Classes</a>
+          <a className="linkFiles">View Files</a>
+        </div>
+        <div className="rightbar"></div>
       </div>
-    </div>
-      <div className="leftbar">
-        <a className="linkClasses">View Classes</a>
-        <a className="linkFiles">View Files</a>
-      </div>
-      <div className="rightbar"></div>
     </div>
   );
 }
